93e552d06ad1cab5d3b8d6ac851841a5835fb793
Pandu Tri Praptomo <122323559+pandutripraptomo@users.noreply.github.com>
2025-05-14 14:57:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ min_date = all_df["dteday"].min()
 max_date = all_df["dteday"].max()
 
 with st.sidebar:
-   
+    st.image("https://raw.githubusercontent.com/pandutripraptomo/main/Analysis_Data_Bike_Sharing_EDA/logo.png")
     start_date, end_date = st.date_input(
         label='Rentang Waktu', min_value=min_date,
         max_value=max_date,
